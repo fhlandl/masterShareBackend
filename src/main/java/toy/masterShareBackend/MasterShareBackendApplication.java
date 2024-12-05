0753ae07c6a67d982e48f071df979ce46d82d7d3
kyungjun_min <kyungjun_min@tmax.co.kr>
2024-12-05 17:10:30 +0900

    프로젝트 생성

---
+++ src/main/java/toy/masterShareBackend/MasterShareBackendApplication.java
@@ -0,0 +1,13 @@
+package toy.masterShareBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterShareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterShareBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toy/masterShareBackend/MasterShareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.masterShareBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterShareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
