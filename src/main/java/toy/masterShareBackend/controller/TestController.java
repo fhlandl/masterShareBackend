ec9f2f4d07db1f537e664c7bc5901a61bfb7d442
kyungjun_min <kyungjun_min@tmax.co.kr>
2024-12-05 18:24:26 +0900

    TestController 추가
    - 프론트엔드에서 서버 호출을 테스트 하기 위한 목적
    - GET, POST 테스트 API 추가

---
+++ src/main/java/toy/masterShareBackend/controller/TestController.java
@@ -0,0 +1,39 @@
+package toy.masterShareBackend.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/connect")
+    public TestResponse connect() {
+
+        return new TestResponse("Get Connection Test", "Success!");
+    }
+
+    @PostMapping("/connect")
+    public TestResponse connect(@RequestBody PostRequest dto) {
+
+        String content = "Hi " + dto.getName() + "! Connection Success!!";
+        return new TestResponse("Post Connection Test", content);
+    }
+
+    @AllArgsConstructor
+    @Getter
+    static class TestResponse {
+        private String title;
+        private String content;
+    }
+
+    @Getter
+    static class PostRequest {
+        private String name;
+    }
+}
